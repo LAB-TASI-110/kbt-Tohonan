1f61a94e7a3ce89f5e610f155915e31b0827542f
Tohonan Silaban <silabandit1@gmail.com>
2026-03-10 14:16:51 +0700

    TOHONAN
    PERBAIKAN

--- KBT.c
+++ KBT.c
@@ -9,7 +9,8 @@
 #define MAX_ALAMAT 20 // Untuk lokasi saat ini
 #define MAX_TUJUAN 20
 #define MAX_KONFIRMASI 5
-#define MAX_NO_MOBIL 5 // Ukuran string untuk nomor mobil (e.g., "M-01")
+#define MAX_NO_MOBIL 7 // Ukuran string untuk nomor mobil (e.g., "KBT-01")
+#define MAX_KELAS 10 // Untuk menyimpan string kelas (Economy/Executive)
 
 int main() {
     // Inisialisasi generator angka acak
@@ -20,10 +21,12 @@ int main() {
     char tgl_berangkat[MAX_TGL];
     char lokasi_saat_ini_str[MAX_ALAMAT];
     char tujuan_str[MAX_TUJUAN];
-    int pilihan_lokasi, pilihan_tujuan;
-    long harga_tiket = 0;
+    int pilihan_lokasi, pilihan_tujuan, pilihan_kelas;
+    long harga_tiket_economy = 0; // Harga dasar untuk kelas Economy
+    long harga_tiket_final = 0;   // Harga akhir setelah penyesuaian kelas
     char konfirmasi_bayar[MAX_KONFIRMASI];
     char no_va[20]; // Nomor Virtual Account
+    char kelas_tiket_str[MAX_KELAS]; // Menyimpan string "ECONOMY" atau "EXECUTIVE"
 
     // Daftar nomor mobil yang tersedia (7 mobil, sesuai minimal 3, maksimal 10)
     const char *daftar_mobil[] = {"KBT-01", "KBT-02", "KBT-03", "KBT-04", "KBT-05", "KBT-06", "KBT-07"};
@@ -59,22 +62,22 @@ int main() {
     scanf("%d", &pilihan_lokasi);
     while (getchar() != '\n'); // Membersihkan buffer input setelah scanf
 
-    // Pilihan tujuan berdasarkan lokasi keberangkatan
+    // Pilihan tujuan berdasarkan lokasi keberangkatan dan penetapan harga Economy
     printf("\nPilih Tujuan Anda:\n");
     switch (pilihan_lokasi) {
         case 1: // Lokasi saat ini: Medan
             strcpy(lokasi_saat_ini_str, "Medan");
-            printf("1. Siborongborong (Rp 90.000)\n");
-            printf("2. Jawa (Rp 200.000)\n");
+            printf("1. Siborongborong\n");
+            printf("2. Jawa\n");
             printf("Masukkan pilihan Anda (1/2): ");
             scanf("%d", &pilihan_tujuan);
             while (getchar() != '\n');
             if (pilihan_tujuan == 1) {
                 strcpy(tujuan_str, "Siborongborong");
-                harga_tiket = 90000;
+                harga_tiket_economy = 90000;
             } else if (pilihan_tujuan == 2) {
                 strcpy(tujuan_str, "Jawa");
-                harga_tiket = 200000;
+                harga_tiket_economy = 200000;
             } else {
                 printf("Pilihan tujuan tidak valid. Program dihentikan.\n");
                 exit(1);
@@ -82,14 +85,14 @@ int main() {
             break;
         case 2: // Lokasi saat ini: Jawa
             strcpy(lokasi_saat_ini_str, "Jawa");
-            printf("1. Medan (Rp 90.000)\n");
+            printf("1. Medan\n");
             // Rute Jawa ke Siborongborong tidak ditawarkan karena harga tidak didefinisikan dalam prompt.
             printf("Masukkan pilihan Anda (1): ");
             scanf("%d", &pilihan_tujuan);
             while (getchar() != '\n');
             if (pilihan_tujuan == 1) {
                 strcpy(tujuan_str, "Medan");
-                harga_tiket = 90000;
+                harga_tiket_economy = 90000;
             } else {
                 printf("Pilihan tujuan tidak valid. Program dihentikan.\n");
                 exit(1);
@@ -97,17 +100,17 @@ int main() {
             break;
         case 3: // Lokasi saat ini: Siborongborong
             strcpy(lokasi_saat_ini_str, "Siborongborong");
-            printf("1. Jawa (Rp 300.000)\n");
-            printf("2. Medan (Rp 90.000)\n");
+            printf("1. Jawa\n");
+            printf("2. Medan\n");
             printf("Masukkan pilihan Anda (1/2): ");
             scanf("%d", &pilihan_tujuan);
             while (getchar() != '\n');
             if (pilihan_tujuan == 1) {
                 strcpy(tujuan_str, "Jawa");
-                harga_tiket = 300000;
+                harga_tiket_economy = 300000;
             } else if (pilihan_tujuan == 2) {
                 strcpy(tujuan_str, "Medan");
-                harga_tiket = 90000;
+                harga_tiket_economy = 90000;
             } else {
                 printf("Pilihan tujuan tidak valid. Program dihentikan.\n");
                 exit(1);
@@ -116,6 +119,25 @@ int main() {
         default:
             printf("Pilihan lokasi keberangkatan tidak valid. Program dihentikan.\n");
             exit(1);
+    }
+
+    // Pilihan kelas KBT
+    printf("\nPilih Kelas Tiket:\n");
+    printf("1. KBT ECONOMY (Harga: Rp %ld)\n", harga_tiket_economy);
+    printf("2. KBT EXECUTIVE (Harga: Rp %ld)\n", (long)(harga_tiket_economy * 1.5));
+    printf("Masukkan pilihan Anda (1/2): ");
+    scanf("%d", &pilihan_kelas);
+    while (getchar() != '\n'); // Membersihkan buffer input setelah scanf
+
+    if (pilihan_kelas == 1) {
+        strcpy(kelas_tiket_str, "ECONOMY");
+        harga_tiket_final = harga_tiket_economy;
+    } else if (pilihan_kelas == 2) {
+        strcpy(kelas_tiket_str, "EXECUTIVE");
+        harga_tiket_final = (long)(harga_tiket_economy * 1.5); // Tambah 50%
+    } else {
+        printf("Pilihan kelas tiket tidak valid. Program dihentikan.\n");
+        exit(1);
     }
 
     // Generate nomor VA sederhana (contoh: KBT-123456789)
@@ -128,9 +150,10 @@ int main() {
     printf("Tanggal Berangkat : %s\n", tgl_berangkat);
     printf("Lokasi Berangkat  : %s\n", lokasi_saat_ini_str);
     printf("Tujuan            : %s\n", tujuan_str);
-    printf("Harga Tiket       : Rp %ld\n", harga_tiket);
+    printf("Kelas Tiket       : KBT %s\n", kelas_tiket_str);
+    printf("Harga Tiket       : Rp %ld\n", harga_tiket_final);
     printf("Nomor Virtual Account (VA) untuk pembayaran: %s\n", no_va);
-    printf("Jumlah yang perlu dibayar: Rp %ld\n", harga_tiket);
+    printf("Jumlah yang perlu dibayar: Rp %ld\n", harga_tiket_final);
 
     // Konfirmasi pembayaran
     printf("\nApakah Anda sudah membayar Nomor VA ini? (ya/tidak): ");
@@ -148,12 +171,12 @@ int main() {
         printf("\n=============================================\n");
         printf("Terima kasih, %s! Tiket Anda telah dikonfirmasi.\n", nama);
         printf("Anda akan mendapatkan konfirmasi lebih lanjut melalui WA: %s.\n", no_wa);
-        printf("Selamat menikmati perjalanan Anda dengan KBT menggunakan mobil %s dari %s ke %s!\n", no_mobil_terpilih, lokasi_saat_ini_str, tujuan_str);
+        printf("Selamat menikmati perjalanan Anda dengan KBT %s menggunakan mobil %s dari %s ke %s!\n", kelas_tiket_str, no_mobil_terpilih, lokasi_saat_ini_str, tujuan_str);
         printf("=============================================\n");
     } else {
         printf("\n=============================================\n");
         printf("Anda harus membayar agar bisa mendapatkan tiket.\n");
-        printf("Silakan lakukan pembayaran ke Nomor VA: %s sejumlah Rp %ld.\n", no_va, harga_tiket);
+        printf("Silakan lakukan pembayaran ke Nomor VA: %s sejumlah Rp %ld.\n", no_va, harga_tiket_final);
         printf("=============================================\n");
     }
 
